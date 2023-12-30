6846c9ecce4560ec67e94f26e920fe455541d64b
shahzaib abid <87558394+shahzaibabid@users.noreply.github.com>
2023-12-30 16:44:56 +0500

    home

--- app.py
+++ app.py
@@ -5,4 +5,12 @@ app = Flask(__name__)
 def index():
  return render_template("index.html")
 
+@app.route("/about")
+def about():
+ return render_template("about-us.html")
+
+@app.route("/contact_us")
+def contact():
+ return render_template("contacts.html")
+
 app.run(debug=True)
