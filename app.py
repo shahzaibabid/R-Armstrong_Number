a4990e6627b01549638ecfaafb83f596d6e659c6
shahzaib abid <87558394+shahzaibabid@users.noreply.github.com>
2024-01-02 17:04:45 +0500

    contact,feedback

--- app.py
+++ app.py
@@ -9,8 +9,12 @@ def index():
 def about():
  return render_template("about-us.html")
 
-@app.route("/contact_us")
+@app.route("/contact")
 def contact():
- return render_template("contacts.html")
+ return render_template("contact.html")
+
+@app.route("/feedback")
+def feedback():
+ return render_template("feedback.html")
 
 app.run(debug=True)
